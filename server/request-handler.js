d2430775f81f691a0e0c4f326772d61b3469e1cb
Hack Reactor Students <jakecadams@gmail.com>
2014-06-23 17:40:52 -0700

    Finished basic requirements. Server and client now working together. Happily.

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,32 +4,91 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+var exports = module.exports = {};
+var storage = [];
+var storageByRoom = {};
+var lastId = 0;
 
-var handleRequest = function(request, response) {
+exports.handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
+  var getRoom = function(){
+    var tempArr = request.url.split('/');
+    var room = undefined;
+    if (tempArr[1] === 'classes' && tempArr[2] === 'room') {
+      room = tempArr[3];
+    }
+    return room;
+  };
+  var handlePostedMessage = function(data){
+    var message = JSON.parse(data);
+
+    message.createdAt = new Date();
+    message.objectId = lastId;
+    lastId++;
+    var room = JSON.stringify(message.roomname);
+    if(!storageByRoom.hasOwnProperty(room)){
+      storageByRoom[room] = [];
+    }
+    var roomOverride = getRoom();
+    if(roomOverride !== undefined){
+      room = roomOverride;
+      message.roomname = room;
+    }
+    storageByRoom[room].push(message);
+    storage.push(message);
+  };
+
+  var responseText = '';
+  var room = getRoom();
 
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   var statusCode = 200;
 
+  if(request.url.match(/\/classes\/messages\??.*/)){
+    if(request.method === 'POST'){
+      statusCode = 201;
+      request.on('data', handlePostedMessage);
+    } else {
+      responseText = JSON.stringify({results:storage});
+      statusCode = 200;
+    }
+  } else if (room !== undefined) {
+    if(request.method === 'POST') {
+      statusCode = 201;
+      request.on('data', handlePostedMessage);
+    } else {
+      if (storageByRoom[room] === undefined) {
+        storageByRoom[room] =[];
+      }
+      responseText = JSON.stringify({results:storageByRoom[room]});
+      statusCode = 200;
+    }
+  } else {
+    statusCode = 404;
+  }
+
+
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
-  var headers = defaultCorsHeaders;
+  var headers = exports.defaultCorsHeaders;
 
   headers['Content-Type'] = "text/plain";
 
   /* .writeHead() tells our server what HTTP status code to send back */
+  console.log('status code: ' + statusCode);
   response.writeHead(statusCode, headers);
 
   /* Make sure to always call response.end() - Node will not send
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+
+  response.end(responseText);
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -37,7 +96,7 @@ var handleRequest = function(request, response) {
  * are on different domains. (Your chat client is running from a url
  * like file://your/chat/client/index.html, which is considered a
  * different domain.) */
-var defaultCorsHeaders = {
+exports.defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
