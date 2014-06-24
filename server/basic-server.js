55596e412524ade22ff02ccb7f69dab8a8e54244
Hack Reactor Students <accounts@hackreactor.com>
2014-06-23 19:33:17 -0700

    Azure. Attempt #3.

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,6 @@
 /* Import node's http module: */
 var http = require("http");
-var requestHandler = require('./request-handler.js');
+// var requestHandler = require('./request-handler.js');
 
 /* Every server needs to listen on a port with a unique number. The
  * standard port for HTTP servers is port 80, but that port is
@@ -8,10 +8,16 @@ var requestHandler = require('./request-handler.js');
  * so we'll use a higher port number that is not likely to be taken: */
 var port = process.env.port || 1337;
 
+
+http.createServer(function(req, res) {
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('Hello World\n');
+}).listen(port);
+
 /* For now, since you're running this server on your local machine,
  * we'll have it listen on the IP address 127.0.0.1, which is a
  * special address that always refers to localhost. */
-var ip = "www.gregtrowbridgechatclienttest.azurewebsites.net";
+// var ip = "www.gregtrowbridgechatclienttest.azurewebsites.net";
 
 
 
@@ -19,9 +25,9 @@ var ip = "www.gregtrowbridgechatclienttest.azurewebsites.net";
 we could have called it anything (myServer, blahblah, etc.). The function we pass it (handleRequest)
 will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in the 'request-handler' file).
 Lastly, we tell the server we made to listen on the given port and IP. */
-var server = http.createServer(requestHandler.handleRequest);
+// var server = http.createServer(requestHandler.handleRequest);
 //console.log("Listening on http://" + ip + ":" + port);
-server.listen(port);
+// server.listen(port);
 //server.listen(port, ip);
 //
 //
